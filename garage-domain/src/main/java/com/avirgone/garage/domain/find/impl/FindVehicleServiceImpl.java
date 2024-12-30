1f0b1fc1b728c0e93b264f43402397bf045e0f43
antoniovirgone <antonaccio86@gmail.com>
2024-12-30 15:40:39 +0100

    complete find vehicle api

--- garage-domain/src/main/java/com/avirgone/garage/domain/find/impl/FindVehicleServiceImpl.java
+++ garage-domain/src/main/java/com/avirgone/garage/domain/find/impl/FindVehicleServiceImpl.java
@@ -3,17 +3,24 @@ package com.avirgone.garage.domain.find.impl;
 import com.avirgone.garage.domain.find.FindVehicleService;
 import com.avirgone.garage.domain.find.model.CustomerCode;
 import com.avirgone.garage.domain.find.model.Vehicle;
+import com.avirgone.garage.domain.find.repository.FindVehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 import java.util.concurrent.CompletionStage;
 
-import static java.util.concurrent.CompletableFuture.completedFuture;
-
 @Service
 public class FindVehicleServiceImpl implements FindVehicleService {
+    private final FindVehicleRepository findVehicleRepository;
+
+    @Autowired
+    public FindVehicleServiceImpl(FindVehicleRepository findVehicleRepository) {
+        this.findVehicleRepository = findVehicleRepository;
+    }
+
     @Override
     public CompletionStage<Set<Vehicle>> find(CustomerCode customerCode) {
-        return completedFuture(Set.of(Vehicle.builder().withCode("abc").withLicensePlate("AB123CD").withColor("black").build()));
+        return findVehicleRepository.find(customerCode);
     }
 }
